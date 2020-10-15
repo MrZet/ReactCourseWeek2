cb42246457c77d80966b8b856dd2e45c59885805
MrZet <tomasz.zezulak@gmail.com>
2020-10-15 22:24:48 +0200

    using classnames library

--- timeboxing/src/components/ProgressBar.js
+++ timeboxing/src/components/ProgressBar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import classnames from 'classnames'
 
 function ProgressBar({percent=33, trackRemaining = false, className="", color = null, big = false})
 {
-    let progressClassName = "progress " + className;
-    if(big){
-        progressClassName += " progress--big"
-    }
-    if(color == "orangered"){
-        progressClassName += " progress--color-orangered"
-    }
+    let progressClassName = classnames(
+        "progress",
+        className,
+        {
+            "progress--big" : big,
+            "progress--color-orangered" : color === "orangered"
+        });
      
     const progressBarWidth = trackRemaining === true ? { width:(100 - percent)+"%"} : { width:percent+"%"};
     const progressBarFloat = trackRemaining === true ? { float: "right" } :  { float: "left" };
