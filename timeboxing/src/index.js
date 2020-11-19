e2e91123744f0e5d70a254e5fd0e31f90a68950c
MrZet <tomasz.zezulak@gmail.com>
2020-11-19 21:26:23 +0100

    enable redux-devtools-extension

--- timeboxing/src/index.js
+++ timeboxing/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import {Provider} from 'react-redux'
 import './styles/main.scss';
 import App from './components/App';
 import {timeboxReducer} from './reducers';
 import thunk from 'redux-thunk'
 
-const store = createStore(timeboxReducer, applyMiddleware(thunk));
+const store = createStore(timeboxReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store = {store}>
